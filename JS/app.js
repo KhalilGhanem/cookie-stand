fa90630a36c4c0fec63734f3bb1acda49b127222
Khalil Ghanem <khalil_ghanem7@yahoo.com>
2021-04-11 16:53:07 +0300

    adding all the stores

--- JS/app.js
+++ JS/app.js
@@ -12,7 +12,6 @@ const Seattle = {
     for(let i=0;i<this.operationHours.length;i++){
       let cookieNumber =getRandom(this.minCustomer,this.maxCustomer);
       cookieNumber=cookieNumber*this.avgCustomer;
-      console.log(cookieNumber);
       this.cookiePerHour.push(cookieNumber);
     }
   },
@@ -24,10 +23,18 @@ const Seattle = {
     let unordered =document.createElement('ul');
     container.appendChild(unordered);
     let li=null;
-    for(let i=0;i<this.operationHours.length;i++){
+    let total=0;
+    for(let i=0;i<=this.operationHours.length;i++){
+      if(i===14){
+        li=document.createElement('li');
+        unordered.appendChild(li);
+        li.textContent=`Total: ${total} cookies`;
+        break;
+      }
       li=document.createElement('li');
       unordered.appendChild(li);
       li.textContent=`${this.operationHours[i]}: ${Math.ceil(this.cookiePerHour[i])} cookies`;
+      total=total+Math.ceil(this.cookiePerHour[i]);
     }
   }
   // AvgCookieSale:function(){
@@ -35,10 +42,182 @@ const Seattle = {
   // }
 
 };
-
 Seattle.randomCustomer();
-console.log('avg the cook'+Seattle.cookiePerHour);
 Seattle.creatingList();
+
+// Tokyo Store
+
+const Tokyo = {
+  minCustomer:3,
+  maxCustomer:24,
+  avgCustomer:1.2,
+  operationHours:['6am','7am','8am','9am','10am','11am','12pm','1pm','2pm','3pm','4pm','5pm','6pm','7pm'],
+  cookiePerHour:[],
+
+  randomCustomer:function(){
+    for(let i=0;i<this.operationHours.length;i++){
+      let cookieNumber =getRandom(this.minCustomer,this.maxCustomer);
+      cookieNumber=cookieNumber*this.avgCustomer;
+      this.cookiePerHour.push(cookieNumber);
+    }
+  },
+  creatingList:function(){
+    let container= document.getElementById('FisrtDiv');
+    let h2=document.createElement('h2');
+    container.appendChild(h2);
+    h2.textContent='Tokyo';
+    let unordered =document.createElement('ul');
+    container.appendChild(unordered);
+    let li=null;
+    let total=0;
+    for(let i=0;i<=this.operationHours.length;i++){
+      if(i===14){
+        li=document.createElement('li');
+        unordered.appendChild(li);
+        li.textContent=`Total: ${total} cookies`;
+        break;
+      }
+      li=document.createElement('li');
+      unordered.appendChild(li);
+      li.textContent=`${this.operationHours[i]}: ${Math.ceil(this.cookiePerHour[i])} cookies`;
+      total=total+Math.ceil(this.cookiePerHour[i]);
+    }
+  }
+
+};
+Tokyo.randomCustomer();
+Tokyo.creatingList();
+
+//Dubai store
+
+const Dubai = {
+  minCustomer:11,
+  maxCustomer:38,
+  avgCustomer:3.7,
+  operationHours:['6am','7am','8am','9am','10am','11am','12pm','1pm','2pm','3pm','4pm','5pm','6pm','7pm'],
+  cookiePerHour:[],
+
+  randomCustomer:function(){
+    for(let i=0;i<this.operationHours.length;i++){
+      let cookieNumber =getRandom(this.minCustomer,this.maxCustomer);
+      cookieNumber=cookieNumber*this.avgCustomer;
+      this.cookiePerHour.push(cookieNumber);
+    }
+  },
+  creatingList:function(){
+    let container= document.getElementById('FisrtDiv');
+    let h2=document.createElement('h2');
+    container.appendChild(h2);
+    h2.textContent='Dubai';
+    let unordered =document.createElement('ul');
+    container.appendChild(unordered);
+    let li=null;
+    let total=0;
+    for(let i=0;i<=this.operationHours.length;i++){
+      if(i===14){
+        li=document.createElement('li');
+        unordered.appendChild(li);
+        li.textContent=`Total: ${total} cookies`;
+        break;
+      }
+      li=document.createElement('li');
+      unordered.appendChild(li);
+      li.textContent=`${this.operationHours[i]}: ${Math.ceil(this.cookiePerHour[i])} cookies`;
+      total=total+Math.ceil(this.cookiePerHour[i]);
+    }
+  }
+
+};
+Dubai.randomCustomer();
+Dubai.creatingList();
+
+// Paris store
+const Paris = {
+  minCustomer:20,
+  maxCustomer:38,
+  avgCustomer:2.3,
+  operationHours:['6am','7am','8am','9am','10am','11am','12pm','1pm','2pm','3pm','4pm','5pm','6pm','7pm'],
+  cookiePerHour:[],
+
+  randomCustomer:function(){
+    for(let i=0;i<this.operationHours.length;i++){
+      let cookieNumber =getRandom(this.minCustomer,this.maxCustomer);
+      cookieNumber=cookieNumber*this.avgCustomer;
+      this.cookiePerHour.push(cookieNumber);
+    }
+  },
+  creatingList:function(){
+    let container= document.getElementById('FisrtDiv');
+    let h2=document.createElement('h2');
+    container.appendChild(h2);
+    h2.textContent='Paris';
+    let unordered =document.createElement('ul');
+    container.appendChild(unordered);
+    let li=null;
+    let total=0;
+    for(let i=0;i<=this.operationHours.length;i++){
+      if(i===14){
+        li=document.createElement('li');
+        unordered.appendChild(li);
+        li.textContent=`Total: ${total} cookies`;
+        break;
+      }
+      li=document.createElement('li');
+      unordered.appendChild(li);
+      li.textContent=`${this.operationHours[i]}: ${Math.ceil(this.cookiePerHour[i])} cookies`;
+      total=total+Math.ceil(this.cookiePerHour[i]);
+    }
+  }
+
+};
+Paris.randomCustomer();
+Paris.creatingList();
+
+//Lima store
+
+const Lima = {
+  minCustomer:2,
+  maxCustomer:16,
+  avgCustomer:4.6,
+  operationHours:['6am','7am','8am','9am','10am','11am','12pm','1pm','2pm','3pm','4pm','5pm','6pm','7pm'],
+  cookiePerHour:[],
+
+  randomCustomer:function(){
+    for(let i=0;i<this.operationHours.length;i++){
+      let cookieNumber =getRandom(this.minCustomer,this.maxCustomer);
+      cookieNumber=cookieNumber*this.avgCustomer;
+      this.cookiePerHour.push(cookieNumber);
+    }
+  },
+  creatingList:function(){
+    let container= document.getElementById('FisrtDiv');
+    let h2=document.createElement('h2');
+    container.appendChild(h2);
+    h2.textContent='Lima';
+    let unordered =document.createElement('ul');
+    container.appendChild(unordered);
+    let li=null;
+    let total=0;
+    for(let i=0;i<=this.operationHours.length;i++){
+      if(i===14){
+        li=document.createElement('li');
+        unordered.appendChild(li);
+        li.textContent=`Total: ${total} cookies`;
+        break;
+      }
+      li=document.createElement('li');
+      unordered.appendChild(li);
+      li.textContent=`${this.operationHours[i]}: ${Math.ceil(this.cookiePerHour[i])} cookies`;
+      total=total+Math.ceil(this.cookiePerHour[i]);
+    }
+  }
+
+};
+Lima.randomCustomer();
+Lima.creatingList();
+
+
+
 
 
 // This funtcion is from The MDN
