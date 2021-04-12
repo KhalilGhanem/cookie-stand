132b37c7bb5a2af4ab72b494c73a5cd8f0b4b5f4
Khalil Ghanem <khalil_ghanem7@yahoo.com>
2021-04-12 15:20:59 +0300

    adding the table

--- JS/app.js
+++ JS/app.js
@@ -1,6 +1,100 @@
-// 'use strict';
+'use strict';
 // eslint-disable-next-line no-unused-vars;
 
+// This funtcion is from The MDN
+function getRandom(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1) + min); //The maximum is inclusive and the minimum is inclusive
+}
+
+
+let operationHours=['6am','7am','8am','9am','10am','11am','12pm','1pm','2pm','3pm','4pm','5pm','6pm','7pm'];
+let container= document.getElementById('FisrtDiv');
+let table=document.createElement('table');
+container.appendChild(table);
+let headerRow = document.createElement('tr');
+table.appendChild(headerRow);
+
+function addheader(){
+
+  let thEl = document.createElement('th');
+  headerRow.appendChild(thEl);
+  thEl.textContent='  `   ';
+  for(let i=0;i<operationHours.length;i++){
+    let thEl1 = document.createElement('th');
+    headerRow.appendChild(thEl1);
+    thEl1.textContent=operationHours[i];
+  }
+  let thEl2=document.createElement('th');
+  headerRow.appendChild(thEl2);
+  thEl2.textContent='Daily Location Total';
+}
+
+function Branch(location,minCustomer,maxCustomer,avgCustomer){
+  this.location=location;
+  this.minCustomer=minCustomer;
+  this.maxCustomer=maxCustomer;
+  this.avgCustomer=avgCustomer;
+  this.cookiePerHour=[];
+  this.cNumber=0;
+}
+
+Branch.prototype.randomCustomer=function(){
+  for(let i=0;i<operationHours.length;i++){
+    let cookieNumber =getRandom(this.minCustomer,this.maxCustomer);
+    cookieNumber=cookieNumber*this.avgCustomer;
+    this.cookiePerHour.push(Math.ceil(cookieNumber));
+    this.cNumber=this.cNumber+Math.ceil(cookieNumber);
+    console.log(cookieNumber);
+  }
+};
+Branch.prototype.render=function(){
+  let tableRow=document.createElement('tr');
+  table.appendChild(tableRow);
+  let tdEl=document.createElement('td');
+  tableRow.appendChild(tdEl);
+  tdEl.textContent=this.location;
+  for(let i=0;i<operationHours.length;i++){
+    let tdEl1=document.createElement('td');
+    tableRow.appendChild(tdEl1);
+    tdEl1.textContent=this.cookiePerHour[i]+' cookies';
+  }
+  let tdEl2=document.createElement('td');
+  tableRow.appendChild(tdEl2);
+  tdEl2.textContent=this.cNumber+' cookies';
+
+
+};
+
+
+addheader();
+let seattle=new Branch('Seattle',23,65,6.3);
+seattle.randomCustomer();
+seattle.render();
+let tokyo= new Branch('Tokyo',3,24,1.2);
+tokyo.randomCustomer();
+tokyo.render();
+let dubai=new Branch('Dubai',11,38,7.3);
+dubai.randomCustomer();
+dubai.render();
+let paris=new Branch('Paris',20,38,2.3);
+paris.randomCustomer();
+paris.render();
+let lima=new Branch('Lima',2,16,4.6);
+lima.randomCustomer();
+lima.render();
+
+//console.log(Seattle.cookiePerHour,Seattle.location);
+
+/*
+Tokyo Store
+
+const Tokyo = {
+  minCustomer:3,
+  maxCustomer:24,
+  avgCustomer:1.2,
+-------
 const Seattle = {
   minCustomer:23,
   maxCustomer:65,
@@ -216,13 +310,8 @@ const Lima = {
 Lima.randomCustomer();
 Lima.creatingList();
 
+*/
 
 
 
 
-// This funtcion is from The MDN
-function getRandom(min, max) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min + 1) + min); //The maximum is inclusive and the minimum is inclusive
-}
